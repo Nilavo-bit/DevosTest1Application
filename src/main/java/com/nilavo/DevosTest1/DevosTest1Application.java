d57151d4e429f0531a9bb032853aa4a3b922f2b6
Nilavodas <nilavodas57@gmail.com>
2025-02-26 01:23:34 +0530

    change date 26-02-2025

--- src/main/java/com/nilavo/DevosTest1/DevosTest1Application.java
+++ src/main/java/com/nilavo/DevosTest1/DevosTest1Application.java
@@ -2,12 +2,44 @@ package com.nilavo.DevosTest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DevosTest1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevosTest1Application.class, args);
+	public static void main(String[] args) throws IOException {
+
+		String inputfile ="D:\\Nilavo\\Mumbai Diary\\input.txt";
+		String outputfile="D:\\Nilavo\\Mumbai Diary\\output.csv";
+
+	    try(BufferedReader reader = new BufferedReader(new FileReader(inputfile));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputfile)))
+		{
+			String line;
+			while((line=reader.readLine())!=null)
+			{
+				writer.write(line);
+				writer.newLine();
+
+			}
+			System.out.println("File copied successfully");
+			System.out.println("File copied successfully");
+
+
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+
+
+
+
+
 	}
 
 }
---
+++ src/test/java/com/nilavo/DevosTest1/controller/mainControllerTest.java
@@ -0,0 +1,8 @@
+package com.nilavo.DevosTest1.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class mainControllerTest {
+
+
+}
