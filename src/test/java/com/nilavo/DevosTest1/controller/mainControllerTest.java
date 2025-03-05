4901ed4eb5c966c29451411c04760337ce9dc7e7
Nilavodas <nilavodas57@gmail.com>
2025-03-06 00:54:09 +0530

    unit testing started

--- src/test/java/com/nilavo/DevosTest1/controller/mainControllerTest.java
+++ src/test/java/com/nilavo/DevosTest1/controller/mainControllerTest.java
@@ -1,8 +1,21 @@
 package com.nilavo.DevosTest1.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class mainControllerTest {
 
+
+    @Test
+    public void firstTest()
+    {
+        mainController mainController = new mainController();
+        ResponseEntity<String> getvalue =mainController.first();
+        assertEquals("Welcome To Main Controller Class:com.nilavo.DevosTest1.controller.mainController",getvalue.getBody());
+        assertEquals(HttpStatus.OK,getvalue.getStatusCode());
+    }
 
 }
